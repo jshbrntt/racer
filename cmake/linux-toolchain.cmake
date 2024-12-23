f96821f1996e69ae14245549a3305d214fd4e28e
Joshua Barnett <joshua@barne.tt>
2024-12-23 21:06:18 +0100

    update linker options so stdlibc++ is statically linked

--- cmake/linux-toolchain.cmake
+++ cmake/linux-toolchain.cmake
@@ -1,2 +1,2 @@
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -lc++")
+# set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
+# set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static-libgcc -static-libstdc++")
